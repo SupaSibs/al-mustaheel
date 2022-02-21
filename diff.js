5f38212d6087bc1c83c4c5a853e30a8d3477fc82
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-02-20 21:27:41 -0800

    Update diff.js

--- diff.js
+++ diff.js
@@ -1,16 +1,1 @@
-function stringToHTML(str) {
-	let parser = new DOMParser();
-	let doc = parser.parseFromString(str, 'text/html');
-	return doc.body;
-}
 
-function render(fn) {
- let tempHTML = stringToHTML(fn())
- 	diff(templateHTML, this.elem);
-}
-
-
-function diff() {
-let domNodes = Array.prototype.slice.call(elem.childNodes);
-let templateNodes = Array.prototype.slice.call(template.childNodes);	
-}
