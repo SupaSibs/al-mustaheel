78da7788278d6d68dcf5c7f08f556262e1d39491
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-02-20 21:29:34 -0800

    Update diff.js

--- diff.js
+++ diff.js
@@ -1,1 +1,10 @@
-
+let support = (function () {
+	if (!window.DOMParser) return false;
+	var parser = new DOMParser();
+	try {
+		parser.parseFromString('x', 'text/html');
+	} catch(err) {
+		return false;
+	}
+	return true;
+})();
